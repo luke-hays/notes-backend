b2fc04569c74078d2abd0464ee550562b2fbd497
Midival <lukehays32@gmail.com>
2023-02-18 23:24:21 -0500

    add api listener functionality

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,33 @@
 import express from 'express'
+import cors from 'cors'
 
 const PORT = 3001
 
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.get('/notes', (req, res, next) => {
+  console.log('get', req)
+  res.send({ test: 'It Works' })
+})
+
+app.post('/notes/', (req, res, next) => {
+  console.log('post', req)
+  res.send({ test: 'It Works' })
+})
+
+app.patch('/notes/:id', (req, res, next) => {
+  console.log('get', req)
+  res.send({ test: 'It Works' })
+})
+
+app.delete('/notes/:id', (req, res, next) => {
+  console.log('get', req)
+  res.send({ test: 'It Works' })
 })
 
 app.listen(PORT, () => {
